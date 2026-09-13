9462ff61c1cd08c13d19bc6cfa878d638c83c4ed
Kwon Yi Jun <kwonyijun13@naver.com>
2026-09-13 12:37:10 +0900

    Added naver api and app exception

---
+++ lib/core/network/naver_api.dart
@@ -0,0 +1,8 @@
+class NaverApi {
+  NaverApi._();
+
+  static const String autocompleteBaseUrl = 'https://ac.stock.naver.com/ac';
+  static const String realtimeBaseUrl = 'https://polling.finance.naver.com/api/realtime';
+  static const String metadataBaseUrl = 'https://stock.naver.com/api/securityFe/api/fchart/domestic/stock';
+  static const String dailyPriceBaseUrl = 'https://finance.naver.com/item/sise_day.naver';
+}
